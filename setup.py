0466a1eb68626c184045ef57fc3020a754417e2e
rr- <mkurczew@gmail.com>
2015-07-04 15:33:46 +0200

    Upgraded to 0.1.1

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name             = 'bk',
     packages         = ['bk'],
-    version          = '0.1',
+    version          = '0.1.1',
     description      = 'An utility that lets you change desktop background for GNU/Linux and Windows.',
     author           = 'rr-',
     author_email     = 'rr-@sakuya.pl',
     url              = 'https://github.com/rr-/bk',
-    download_url     = 'https://github.com/rr-/bk/tarball/0.1',
+    download_url     = 'https://github.com/rr-/bk/tarball/0.1.1',
     keywords         = ['wallpaper', 'screen', 'monitor', 'desktop'],
     install_requires = ['screeninfo'],
 
