1104576d06edd14eecd507a412d22a24cc64b556
rr- <mkurczew@gmail.com>
2015-07-04 15:44:41 +0200

    Fixed path to main executable

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from setuptools import setup
 setup(
     name             = 'bk',
     packages         = ['bk'],
-    version          = '0.1.1',
+    version          = '0.1.2',
     description      = 'An utility that lets you change desktop background for GNU/Linux and Windows.',
     author           = 'rr-',
     author_email     = 'rr-@sakuya.pl',
     url              = 'https://github.com/rr-/bk',
-    download_url     = 'https://github.com/rr-/bk/tarball/0.1.1',
+    download_url     = 'https://github.com/rr-/bk/tarball/0.1.2',
     keywords         = ['wallpaper', 'screen', 'monitor', 'desktop'],
     install_requires = ['screeninfo'],
 
     entry_points = {
         'console_scripts': [
-            'bk = bk.bk:main',
+            'bk = bk:main',
         ],
     },
     classifiers      = [],
