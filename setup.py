c8f9caa83b3e044eb4e1f1ffdd3dacbd0dfb2287
rr- <mkurczew@gmail.com>
2015-07-04 15:53:53 +0200

    Added pillow to required dependencies

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(
     name             = 'bk',
     packages         = ['bk'],
-    version          = '0.1.3',
+    version          = '0.1.4',
     description      = 'An utility that lets you change desktop background for GNU/Linux and Windows.',
     author           = 'rr-',
     author_email     = 'rr-@sakuya.pl',
     url              = 'https://github.com/rr-/bk',
-    download_url     = 'https://github.com/rr-/bk/tarball/0.1.3',
+    download_url     = 'https://github.com/rr-/bk/tarball/0.1.4',
     keywords         = ['wallpaper', 'screen', 'monitor', 'desktop'],
-    install_requires = ['screeninfo'],
+    install_requires = ['screeninfo', 'pillow'],
     scripts          = ['bk/bk'],
     classifiers      = [],
 )
